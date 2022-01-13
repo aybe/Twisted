9418ba9e9f33a0461ba4017dd8dab6d82d7bb489
aybe <aybe@users.noreply.github.com>
2022-01-13 06:37:59 +0100

    More polygon reading...

--- Twisted.Tests/PC/DPCNode00FFXXXX.cs
+++ Twisted.Tests/PC/DPCNode00FFXXXX.cs
@@ -17,6 +17,10 @@ public sealed class DPCNode00FFXXXX : DPCNode
 
         // TODO vertices, normals, polygons ???
 
+        Vertices = addresses[0];
+        Normals  = addresses[1];
+        Polygons = addresses[2];
+
         var b1 = reader.ReadByte();
         var b2 = reader.ReadByte();
         var b3 = reader.ReadByte();
@@ -35,48 +39,231 @@ public sealed class DPCNode00FFXXXX : DPCNode
 
         for (var i = 0; i < b1; i++)
         {
-            var position = reader.Position;
-
             var peek = reader.Peek(s => s.ReadUInt32(Endianness.BE));
+            var open = reader.Position;
 
             switch (peek)
             {
                 case 0x00010604:
-                    reader.ReadBytes(24); // TODO maybe
-                    break;
                 case 0x00010605:
-                    reader.ReadBytes(24); // TODO maybe
-                    break;
+                {
+                    var bytes = reader.ReadBytes(24); // TODO maybe
+
+                    var v1 = bytes.ReadInt16(4,  Endianness.LE);
+                    var v2 = bytes.ReadInt16(6,  Endianness.LE);
+                    var v3 = bytes.ReadInt16(8,  Endianness.LE);
+                    var v4 = bytes.ReadInt16(10, Endianness.LE);
+
+                    var position = reader.Position;
+
+                    foreach (var index in new[] { v1, v2, v3, v4 })
+                    {
+                        reader.Position = Vertices + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    reader.Position = position;
+                    continue;
+                }
                 case 0x00010705:
-                    reader.ReadBytes(28); // TODO maybe
-                    break;
+                {
+                    var bytes = reader.ReadBytes(28); // TODO maybe
+
+                    var v1 = bytes.ReadInt16(4,  Endianness.LE);
+                    var v2 = bytes.ReadInt16(6,  Endianness.LE);
+                    var v3 = bytes.ReadInt16(8,  Endianness.LE);
+                    var v4 = bytes.ReadInt16(10, Endianness.LE);
+
+                    var position = reader.Position;
+
+                    foreach (var index in new[] { v1, v2, v3, v4 })
+                    {
+                        reader.Position = Vertices + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    reader.Position = position;
+                    continue;
+                }
                 case 0x00010806:
-                    reader.ReadBytes(32); // TODO maybe
-                    break;
                 case 0x00010808:
-                    reader.ReadBytes(32); // TODO maybe
-                    break;
-                case 0x03010A07:
-                    reader.ReadBytes(40); // TODO maybe
-                    break;
+                {
+                    var bytes = reader.ReadBytes(32); // TODO maybe
+
+                    var v1 = bytes.ReadInt16(4,  Endianness.LE);
+                    var v2 = bytes.ReadInt16(6,  Endianness.LE);
+                    var v3 = bytes.ReadInt16(8,  Endianness.LE);
+                    var v4 = bytes.ReadInt16(10, Endianness.LE);
+
+                    var position = reader.Position;
+
+                    foreach (var index in new[] { v1, v2, v3, v4 })
+                    {
+                        reader.Position = Vertices + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    var n1 = bytes.ReadInt16(24, Endianness.LE);
+                    var n2 = bytes.ReadInt16(26, Endianness.LE);
+                    var n3 = bytes.ReadInt16(28, Endianness.LE);
+                    var n4 = bytes.ReadInt16(30, Endianness.LE);
+
+                    foreach (var index in new[] { n1, n2, n3, n4 })
+                    {
+                        reader.Position = Normals + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    reader.Position = position;
+                    continue;
+                }
+                case 0x03010A07: // TODO are these the double-faced polygons? 
+                {
+                    var bytes = reader.ReadBytes(40); // TODO maybe
+
+                    var v1 = bytes.ReadInt16(4, Endianness.LE);
+                    var v2 = bytes.ReadInt16(6, Endianness.LE);
+                    var v3 = bytes.ReadInt16(8, Endianness.LE);
+
+                    var position = reader.Position;
+
+                    foreach (var index in new[] { v1, v2, v3 })
+                    {
+                        reader.Position = Vertices + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    var n1 = bytes.ReadInt16(24, Endianness.LE);
+                    var n2 = bytes.ReadInt16(26, Endianness.LE);
+                    var n3 = bytes.ReadInt16(28, Endianness.LE);
+                    var n4 = bytes.ReadInt16(30, Endianness.LE);
+                    var n5 = bytes.ReadInt16(32, Endianness.LE);
+                    var n6 = bytes.ReadInt16(34, Endianness.LE);
+
+                    foreach (var index in new[] { n1, n2, n3, n4, n5, n6 })
+                    {
+                        reader.Position = Normals + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    reader.Position = position;
+                    continue;
+                }
                 case 0x04010B09:
-                    reader.ReadBytes(44); // TODO maybe
-                    break;
+                {
+                    var bytes = reader.ReadBytes(44); // TODO maybe
+
+                    var v1 = bytes.ReadInt16(4,  Endianness.LE);
+                    var v2 = bytes.ReadInt16(6,  Endianness.LE);
+                    var v3 = bytes.ReadInt16(8,  Endianness.LE);
+                    var v4 = bytes.ReadInt16(10, Endianness.LE);
+
+                    var position = reader.Position;
+
+                    foreach (var index in new[] { v1, v2, v3, v4 })
+                    {
+                        reader.Position = Vertices + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    reader.Position = position;
+                    continue;
+                }
                 case 0x04010C09:
-                    reader.ReadBytes(48); // TODO maybe
-                    break;
-                case 0x83010B07:
-                    reader.ReadBytes(44); // TODO maybe
-                    break;
+                {
+                    var bytes = reader.ReadBytes(48); // TODO maybe
+
+                    var v1 = bytes.ReadInt16(4,  Endianness.LE);
+                    var v2 = bytes.ReadInt16(6,  Endianness.LE);
+                    var v3 = bytes.ReadInt16(8,  Endianness.LE);
+                    var v4 = bytes.ReadInt16(10, Endianness.LE);
+
+                    var position = reader.Position;
+
+                    foreach (var index in new[] { v1, v2, v3, v4 })
+                    {
+                        reader.Position = Vertices + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    reader.Position = position;
+                    continue;
+                }
+                case 0x83010B07: // TODO 0x000000E2 at end
+                {
+                    var bytes = reader.ReadBytes(44); // TODO maybe
+
+                    var v1 = bytes.ReadInt16(4, Endianness.LE);
+                    var v2 = bytes.ReadInt16(6, Endianness.LE);
+                    var v3 = bytes.ReadInt16(8, Endianness.LE);
+
+                    var position = reader.Position;
+
+                    foreach (var index in new[] { v1, v2, v3 })
+                    {
+                        reader.Position = Vertices + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    reader.Position = position;
+                    continue;
+                }
                 case 0x84010C09:
-                    reader.ReadBytes(48); // TODO maybe
-                    break;
+                {
+                    var bytes = reader.ReadBytes(48); // TODO maybe
+
+                    var v1 = bytes.ReadInt16(4,  Endianness.LE);
+                    var v2 = bytes.ReadInt16(6,  Endianness.LE);
+                    var v3 = bytes.ReadInt16(8,  Endianness.LE);
+                    var v4 = bytes.ReadInt16(10, Endianness.LE);
+
+                    var position = reader.Position;
+
+                    foreach (var index in new[] { v1, v2, v3, v4 })
+                    {
+                        reader.Position = Vertices + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    reader.Position = position;
+                    continue;
+                }
                 case 0x84010D09:
-                    reader.ReadBytes(52); // TODO maybe
-                    break;
+                {
+                    var bytes = reader.ReadBytes(52); // TODO maybe
+
+                    var v1 = bytes.ReadInt16(4,  Endianness.LE);
+                    var v2 = bytes.ReadInt16(6,  Endianness.LE);
+                    var v3 = bytes.ReadInt16(8,  Endianness.LE);
+                    var v4 = bytes.ReadInt16(10, Endianness.LE);
+
+                    var position = reader.Position;
+
+                    foreach (var index in new[] { v1, v2, v3, v4 })
+                    {
+                        reader.Position = Vertices + index * 8;
+                        reader.ReadBytes(8);
+                    }
+
+                    reader.Position = position;
+                    continue;
+                }
                 default:
-                    throw new NotSupportedException($"Unknown polygon 0x{peek:X8} @ {position}.");
+                    throw new NotSupportedException($"Unknown polygon 0x{peek:X8} @ {reader.Position}.");
             }
+
+            Console.WriteLine($"Unprocessed polygon 0x{peek:X8} @ {open}.");
         }
+    }
+
+    public int Vertices { get; }
+
+    public int Normals { get; }
+
+    public int Polygons { get; }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, {nameof(Vertices)}: {Vertices}, {nameof(Normals)}: {Normals}, {nameof(Polygons)}: {Polygons}";
     }
 }
