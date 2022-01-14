1797b4a543fafad8e91445d675215c0b2c1f914d
aybe <aybe@users.noreply.github.com>
2022-01-14 22:30:26 +0100

    Add new node XXXXXXXX

---
+++ Twisted.Tests/PC/DPCNodeXXXXXXXX.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Twisted.Tests.PC;
+
+public sealed class DPCNodeXXXXXXXX : DPCNode
+{
+    internal DPCNodeXXXXXXXX(DPCNodeReader reader, out int[] children) : base(reader)
+    {
+        var bytes = reader.ReadBytes(8);
+
+        var i1 = bytes.ReadInt32(4, Endianness.LE);
+
+        // Assert.AreEqual(0, i1, Position.ToString());
+
+        var addresses = reader.ReadAddresses(1);
+
+        children = addresses;
+    }
+}
