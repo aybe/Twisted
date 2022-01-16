4b92845deee6f7d2d00bc9213dd9671783a40947
aybe <aybe@users.noreply.github.com>
2022-01-16 06:49:07 +0100

    Restore DMD tests

---
+++ Twisted/PS/DMDTester.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Twisted.PS;
+
+public static class DMDTester
+{
+    public static void Test(TestContext context, string path)
+    {
+        using var stream = new MemoryStream(File.ReadAllBytes(path));
+
+        var dmd = new DMD(stream);
+
+        context.WriteLine(dmd.BaseAddress.ToString());
+    }
+}
