3d6249d4e856497fc473cb87e1bdaa1c47767606
aybe <aybe@users.noreply.github.com>
2022-01-19 03:37:40 +0100

    0010 doesn't makes sense at all, should be data?

--- Twisted/PS/V2/DMDNode0010.cs
+++ Twisted/PS/V2/DMDNode0010.cs
@@ -8,6 +8,6 @@ public sealed class DMDNode0010 : DMDNode
         if (reader == null)
             throw new ArgumentNullException(nameof(reader));
 
-        var unknown2 = reader.ReadBytes(44);
+        // var unknown2 = reader.ReadBytes(44); // BUG this doesn't makes sense at all
     }
 }
