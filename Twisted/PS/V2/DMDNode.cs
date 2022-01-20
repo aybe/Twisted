9c69c7368754531339dfc33fa4cc64d94f66c9eb
aybe <aybe@users.noreply.github.com>
2022-01-20 10:51:17 +0100

    Convert switch expression for code coverage

--- Twisted/PS/V2/DMDNode.cs
+++ Twisted/PS/V2/DMDNode.cs
@@ -68,6 +68,7 @@ public abstract class DMDNode : TreeNode
     }
 
     [SuppressMessage("ReSharper", "UnusedMethodReturnValue.Local")]
+    [SuppressMessage("ReSharper", "ConvertSwitchStatementToSwitchExpression", Justification = "Code coverage")]
     private static DMDNode ReadNode(DMDNode? parent, BinaryReader reader)
     {
         if (reader == null)
@@ -91,88 +92,70 @@ public abstract class DMDNode : TreeNode
          * anything else is bad op code
          */
 
-        DMDNode node = peek switch
+        switch (peek)
         {
-            // TODO are these geometry?
-
-            0x0010 => new DMDNode0010(parent, reader),
-            0x00F0 => new DMDNode0010(parent, reader),
-
-            // TODO this geometry stuff should be refactored
-
-            0x0009 => new DMDNodeXXXX(parent, reader),
-            0x0019 => new DMDNodeXXXX(parent, reader),
-            0x0040 => new DMDNodeXXXX(parent, reader),
-            0x0064 => new DMDNodeXXXX(parent, reader),
-            0x0071 => new DMDNodeXXXX(parent, reader),
-            0x00E1 => new DMDNodeXXXX(parent, reader),
-            0x00E4 => new DMDNodeXXXX(parent, reader),
-            0x00F9 => new DMDNodeXXXX(parent, reader),
-            0x093D => new DMDNodeXXXX(parent, reader),
-            0x0417 => new DMDNodeXXXX(parent, reader),
-            0x00C4 => new DMDNodeXXXX(parent, reader),
-            0x1027 => new DMDNodeXXXX(parent, reader),
-            0x1055 => new DMDNodeXXXX(parent, reader),
-            0x105C => new DMDNodeXXXX(parent, reader),
-            0x105E => new DMDNodeXXXX(parent, reader),
-            0x107A => new DMDNodeXXXX(parent, reader),
-            0x108C => new DMDNodeXXXX(parent, reader),
-            0x24F4 => new DMDNodeXXXX(parent, reader),
-            0x409C => new DMDNodeXXXX(parent, reader),
-            0x4070 => new DMDNodeXXXX(parent, reader),
-            0x407E => new DMDNodeXXXX(parent, reader),
-            0x4042 => new DMDNodeXXXX(parent, reader),
-            0x4038 => new DMDNodeXXXX(parent, reader),
-            0x40E8 => new DMDNodeXXXX(parent, reader),
-            0x40F4 => new DMDNodeXXXX(parent, reader),
-            0x4495 => new DMDNodeXXXX(parent, reader),
-            0x4406 => new DMDNodeXXXX(parent, reader),
-            0x44D2 => new DMDNodeXXXX(parent, reader),
-            0x5125 => new DMDNodeXXXX(parent, reader),
-            0x5195 => new DMDNodeXXXX(parent, reader),
-            0x643F => new DMDNodeXXXX(parent, reader),
-            0x6472 => new DMDNodeXXXX(parent, reader),
-            0x6927 => new DMDNodeXXXX(parent, reader),
-            0x84DE => new DMDNodeXXXX(parent, reader),
-            0x904A => new DMDNodeXXXX(parent, reader),
-            0x90D0 => new DMDNodeXXXX(parent, reader),
-            0x90C9 => new DMDNodeXXXX(parent, reader),
-            0x905F => new DMDNodeXXXX(parent, reader),
-            0x9076 => new DMDNodeXXXX(parent, reader),
-            0x907E => new DMDNodeXXXX(parent, reader),
-            0x90EB => new DMDNodeXXXX(parent, reader),
-            0xC1C5 => new DMDNodeXXXX(parent, reader),
-            0xA177 => new DMDNodeXXXX(parent, reader),
-            0xE457 => new DMDNodeXXXX(parent, reader),
-
-            0x00FF => new DMDNode00FF(parent, reader),
-
-            0x0107 => new DMDNode0107(parent, reader),
-
-            0x0206 => new DMDNode020X(parent, reader),
-            0x0207 => new DMDNode020X(parent, reader),
-            0x0208 => new DMDNode020X(parent, reader),
-            0x0209 => new DMDNode020X(parent, reader),
-            0x020A => new DMDNode020X(parent, reader),
-
-            0x0305 => new DMDNode0305(parent, reader),
-
-            0x040B => new DMDNode040B(parent, reader),
-
-            0x050B => new DMDNode050B(parent, reader),
-
-            0x07FF => new DMDNode07FF(parent, reader),
-
-            0x08FF => new DMDNode08FF(parent, reader),
-
-            0x0903 => new DMDNode0903(parent, reader),
-
-            0x0B06 => new DMDNode0B06(parent, reader),
-
-            _ => throw new NotSupportedException($"{nameof(NodeType)} = 0x{peek:X4}, {nameof(Position)} = {position}")
-        };
-
-        return node;
+            case 0x0010: return new DMDNode0010(parent, reader);
+            case 0x00F0: return new DMDNode0010(parent, reader);
+            case 0x0009: return new DMDNodeXXXX(parent, reader);
+            case 0x0019: return new DMDNodeXXXX(parent, reader);
+            case 0x0040: return new DMDNodeXXXX(parent, reader);
+            case 0x0064: return new DMDNodeXXXX(parent, reader);
+            case 0x0071: return new DMDNodeXXXX(parent, reader);
+            case 0x00E1: return new DMDNodeXXXX(parent, reader);
+            case 0x00E4: return new DMDNodeXXXX(parent, reader);
+            case 0x00F9: return new DMDNodeXXXX(parent, reader);
+            case 0x093D: return new DMDNodeXXXX(parent, reader);
+            case 0x0417: return new DMDNodeXXXX(parent, reader);
+            case 0x00C4: return new DMDNodeXXXX(parent, reader);
+            case 0x1027: return new DMDNodeXXXX(parent, reader);
+            case 0x1055: return new DMDNodeXXXX(parent, reader);
+            case 0x105C: return new DMDNodeXXXX(parent, reader);
+            case 0x105E: return new DMDNodeXXXX(parent, reader);
+            case 0x107A: return new DMDNodeXXXX(parent, reader);
+            case 0x108C: return new DMDNodeXXXX(parent, reader);
+            case 0x24F4: return new DMDNodeXXXX(parent, reader);
+            case 0x409C: return new DMDNodeXXXX(parent, reader);
+            case 0x4070: return new DMDNodeXXXX(parent, reader);
+            case 0x407E: return new DMDNodeXXXX(parent, reader);
+            case 0x4042: return new DMDNodeXXXX(parent, reader);
+            case 0x4038: return new DMDNodeXXXX(parent, reader);
+            case 0x40E8: return new DMDNodeXXXX(parent, reader);
+            case 0x40F4: return new DMDNodeXXXX(parent, reader);
+            case 0x4495: return new DMDNodeXXXX(parent, reader);
+            case 0x4406: return new DMDNodeXXXX(parent, reader);
+            case 0x44D2: return new DMDNodeXXXX(parent, reader);
+            case 0x5125: return new DMDNodeXXXX(parent, reader);
+            case 0x5195: return new DMDNodeXXXX(parent, reader);
+            case 0x643F: return new DMDNodeXXXX(parent, reader);
+            case 0x6472: return new DMDNodeXXXX(parent, reader);
+            case 0x6927: return new DMDNodeXXXX(parent, reader);
+            case 0x84DE: return new DMDNodeXXXX(parent, reader);
+            case 0x904A: return new DMDNodeXXXX(parent, reader);
+            case 0x90D0: return new DMDNodeXXXX(parent, reader);
+            case 0x90C9: return new DMDNodeXXXX(parent, reader);
+            case 0x905F: return new DMDNodeXXXX(parent, reader);
+            case 0x9076: return new DMDNodeXXXX(parent, reader);
+            case 0x907E: return new DMDNodeXXXX(parent, reader);
+            case 0x90EB: return new DMDNodeXXXX(parent, reader);
+            case 0xC1C5: return new DMDNodeXXXX(parent, reader);
+            case 0xA177: return new DMDNodeXXXX(parent, reader);
+            case 0xE457: return new DMDNodeXXXX(parent, reader);
+            case 0x00FF: return new DMDNode00FF(parent, reader);
+            case 0x0107: return new DMDNode0107(parent, reader);
+            case 0x0206: return new DMDNode020X(parent, reader);
+            case 0x0207: return new DMDNode020X(parent, reader);
+            case 0x0208: return new DMDNode020X(parent, reader);
+            case 0x0209: return new DMDNode020X(parent, reader);
+            case 0x020A: return new DMDNode020X(parent, reader);
+            case 0x0305: return new DMDNode0305(parent, reader);
+            case 0x040B: return new DMDNode040B(parent, reader);
+            case 0x050B: return new DMDNode050B(parent, reader);
+            case 0x07FF: return new DMDNode07FF(parent, reader);
+            case 0x08FF: return new DMDNode08FF(parent, reader);
+            case 0x0903: return new DMDNode0903(parent, reader);
+            case 0x0B06: return new DMDNode0B06(parent, reader);
+            default: throw new NotSupportedException($"{nameof(NodeType)} = 0x{peek:X4}, {nameof(Position)} = {position}");
+        }
     }
 
     protected static DMDNode[] ReadNodes(DMDNode? parent, BinaryReader reader, uint[] addresses)
