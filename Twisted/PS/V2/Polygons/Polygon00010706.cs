a75eaed6b04712341aca03925ab82871871173a3
aybe <aybe@users.noreply.github.com>
2022-01-21 10:55:55 +0100

    Read normals for 0x00010706

--- Twisted/PS/V2/Polygons/Polygon00010706.cs
+++ Twisted/PS/V2/Polygons/Polygon00010706.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twisted.Extensions;
 
 namespace Twisted.PS.V2.Polygons;
 
@@ -6,7 +7,7 @@ internal sealed class Polygon00010706 : PolygonQuad
 {
     private byte[] Bytes;
 
-    public Polygon00010706(BinaryReader reader, int positionVertices) : base(reader, positionVertices, 3)
+    public Polygon00010706(BinaryReader reader, int positionVertices, int positionNormals) : base(reader, positionVertices, 3)
     {
         if (reader == null)
             throw new ArgumentNullException(nameof(reader));
@@ -14,5 +15,11 @@ internal sealed class Polygon00010706 : PolygonQuad
         Assert.AreEqual(0, Indices[3], "Triangle expected.");
 
         Bytes = reader.ReadBytes(16);
+        
+        var normal1 = Bytes.ReadUInt16(8, Endianness.LE);
+        var normal2 = Bytes.ReadUInt16(10, Endianness.LE);
+        var normal3 = Bytes.ReadUInt16(12, Endianness.LE);
+
+        ReadNormals(reader, positionNormals, normal1, normal2, normal3);
     }
 }
