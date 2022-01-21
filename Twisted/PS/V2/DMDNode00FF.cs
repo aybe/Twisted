c1059421ef8bce8ff4266755413a820e0f712d92
aybe <aybe@users.noreply.github.com>
2022-01-21 04:19:45 +0100

    Code style for 00FF

--- Twisted/PS/V2/DMDNode00FF.cs
+++ Twisted/PS/V2/DMDNode00FF.cs
@@ -1,17 +1,17 @@
-﻿using JetBrains.Annotations;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Twisted.Extensions;
+﻿using Twisted.Extensions;
 using Twisted.PS.V2.Polygons;
 
 namespace Twisted.PS.V2;
 
-[NoReorder]
 public sealed class DMDNode00FF : DMDNode
 {
-    public readonly uint                    VerticesOffset;
-    public readonly uint                    NormalOffset;
-    public readonly uint                    PolygonsOffset;
-    public readonly IReadOnlyList<IPolygon> Polygons;
+    public uint PositionVertices { get; }
+
+    public uint PositionNormals { get; }
+
+    public uint PositionPolygons { get; }
+
+    public IReadOnlyList<IPolygon> Polygons { get; }
 
     public byte Flags { get; }
 
@@ -21,27 +21,34 @@ public sealed class DMDNode00FF : DMDNode
         if (reader == null)
             throw new ArgumentNullException(nameof(reader));
 
-        VerticesOffset = ReadAddress(reader);
-        NormalOffset   = ReadAddress(reader, false); // TODO could it be that out of bounds signifies no normals?
-        PolygonsOffset = ReadAddress(reader);
+        PositionVertices = ReadAddress(reader);
+        PositionNormals  = ReadAddress(reader, false); // TODO could it be that out of bounds signifies no normals?
+        PositionPolygons = ReadAddress(reader);
 
         var bytes = reader.ReadBytes(28);
 
         Flags = bytes[11];
 
-        // Assert.IsTrue((Flags & 0x80) != 0, Position.ToString());
-
         if ((Flags & 0x80) != 0)
         {
-            var bytes3 = reader.ReadBytes(32 + 16); // TODO 00 10 00 00 .. .. .. .., 00 01 05 05 .. .. .. ..
+            var extraBytes = reader.ReadBytes(32 + 16); // TODO 00 10 00 00 .. .. .. .., 00 01 05 05 .. .. .. .. 
         }
 
         SetLength(reader);
 
         var count = bytes.ReadUInt16(0, Endianness.LE);
 
-        reader.BaseStream.Position = PolygonsOffset;
+        reader.BaseStream.Position = PositionPolygons;
 
-        Polygons = PolygonReader.TryRead(reader, count, VerticesOffset, NormalOffset);
+        Polygons = PolygonReader.TryRead(reader, count, PositionVertices, PositionNormals);
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, " +
+               $"{nameof(PositionVertices)}: {PositionVertices}, " +
+               $"{nameof(PositionNormals)}: {PositionNormals}, " +
+               $"{nameof(PositionPolygons)}: {PositionPolygons}, " +
+               $"{nameof(Polygons)}: {Polygons.Count}";
     }
 }
