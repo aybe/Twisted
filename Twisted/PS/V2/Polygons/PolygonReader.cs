d1daec8f2bd24e9facce30e335ea64ab298837ff
aybe <aybe@users.noreply.github.com>
2022-01-21 10:33:51 +0100

    Read normals for 0x04010B0C

--- Twisted/PS/V2/Polygons/PolygonReader.cs
+++ Twisted/PS/V2/Polygons/PolygonReader.cs
@@ -82,7 +82,7 @@ internal static class PolygonReader
             case 0x03030C09: return new Polygon03030C09(reader, vertices.ToInt32());
             case 0x04010909: return new Polygon04010909(reader, vertices.ToInt32());
             case 0x04010A09: return new Polygon04010A09(reader, vertices.ToInt32());
-            case 0x04010B0C: return new Polygon04010B0C(reader, vertices.ToInt32());
+            case 0x04010B0C: return new Polygon04010B0C(reader, vertices.ToInt32(), normals.ToInt32());
             case 0x04040C0C: return new Polygon04040C0C(reader, vertices.ToInt32());
             case 0x04040D0C: return new Polygon04040D0C(reader, vertices.ToInt32());
             case 0x04040E0C: return new Polygon04040E0C(reader, vertices.ToInt32());
