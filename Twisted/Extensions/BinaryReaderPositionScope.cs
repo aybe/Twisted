6425419b547fa2299fc06473b107d943032481b5
aybe <aybe@users.noreply.github.com>
2022-01-21 10:32:11 +0100

    Create BinaryReaderPositionScope.cs

---
+++ Twisted/Extensions/BinaryReaderPositionScope.cs
@@ -0,0 +1,19 @@
+﻿namespace Twisted.Extensions;
+
+public readonly struct BinaryReaderPositionScope : IDisposable
+{
+    private BinaryReader Reader { get; }
+
+    private long Position { get; }
+
+    public BinaryReaderPositionScope(BinaryReader reader)
+    {
+        Reader   = reader ?? throw new ArgumentNullException(nameof(reader));
+        Position = reader.BaseStream.Position;
+    }
+
+    public void Dispose()
+    {
+        Reader.BaseStream.Position = Position;
+    }
+}
