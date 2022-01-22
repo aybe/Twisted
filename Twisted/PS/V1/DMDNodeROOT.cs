ccdae70eb88e7b491ecdc21fbf72353de605d967
aybe <aybe@users.noreply.github.com>
2022-01-22 06:19:12 +0100

    Remove first version of DMD implementation

--- Twisted/PS/V1/DMDNodeROOT.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Twisted.PS.V1;
-
-[Obsolete(null, true)]
-public sealed class DMDNodeROOT : DMDNode
-{
-    public DMDNodeROOT(DMD dmd, DMDNode? parent) : base(dmd, parent)
-    {
-    }
-}
