fe28ce22deb05602312d04822b6f0dda11584994
aybe <aybe@users.noreply.github.com>
2022-01-24 10:15:42 +0100

    Massive refactoring of test outcome onto static methods

---
+++ Twisted/Extensions/DelegateComparer.cs
@@ -0,0 +1,16 @@
+﻿namespace Twisted.Extensions;
+
+public sealed class DelegateComparer<T> : Comparer<T>
+{
+    public DelegateComparer(Func<T?, T?, int> compareFunc)
+    {
+        CompareFunc = compareFunc ?? throw new ArgumentNullException(nameof(compareFunc));
+    }
+
+    private Func<T?, T?, int> CompareFunc { get; }
+
+    public override int Compare(T? x, T? y)
+    {
+        return CompareFunc(x, y);
+    }
+}
