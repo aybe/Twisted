d5b22be75a164c5f3d11fa2a6db9451e47917d69
aybe <aybe@users.noreply.github.com>
2022-01-24 13:33:23 +0100

    Create DelegateEqualityComparer.cs

---
+++ Twisted/Extensions/DelegateEqualityComparer.cs
@@ -0,0 +1,24 @@
+﻿namespace Twisted.Extensions;
+
+public sealed class DelegateEqualityComparer<T> : EqualityComparer<T>
+{
+    public DelegateEqualityComparer(Func<T?, T?, bool> equals, Func<T, int>? getHashCode = default)
+    {
+        EqualsFunc      = equals ?? throw new ArgumentNullException(nameof(equals));
+        GetHashCodeFunc = getHashCode ?? (s => s?.GetHashCode() ?? default);
+    }
+
+    private Func<T?, T?, bool> EqualsFunc { get; }
+
+    private Func<T, int> GetHashCodeFunc { get; }
+
+    public override bool Equals(T? x, T? y)
+    {
+        return EqualsFunc(x, y);
+    }
+
+    public override int GetHashCode(T obj)
+    {
+        return GetHashCodeFunc(obj);
+    }
+}
