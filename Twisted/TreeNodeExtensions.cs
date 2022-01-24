93fe1b4e077b64d5bf758bac515d74c0563a06b6
aybe <aybe@users.noreply.github.com>
2022-01-24 07:12:37 +0100

    Add more traverse extension methods

--- Twisted/TreeNodeExtensions.cs
+++ Twisted/TreeNodeExtensions.cs
@@ -3,6 +3,28 @@
 public static class TreeNodeExtensions
     // NOTE having these as extensions removes the need from specifying T in caller
 {
+    public static IEnumerable<T> TraverseBfs<T>(this T node) where T : TreeNode
+    {
+        if (node == null)
+            throw new ArgumentNullException(nameof(node));
+
+        var queue = new Queue<T>();
+
+        queue.Enqueue(node);
+
+        while (queue.Any())
+        {
+            var dequeue = queue.Dequeue();
+
+            yield return dequeue;
+
+            foreach (var item in dequeue)
+            {
+                queue.Enqueue((T)item);
+            }
+        }
+    }
+
     public static void TraverseBfs<T>(this T node, Action<T> visitor) where T : TreeNode
     {
         if (node == null)
@@ -24,6 +46,28 @@ public static class TreeNodeExtensions
             foreach (var item in dequeue)
             {
                 queue.Enqueue((T)item);
+            }
+        }
+    }
+
+    public static IEnumerable<T> TraverseDfsPreOrder<T>(this T node) where T : TreeNode
+    {
+        if (node == null)
+            throw new ArgumentNullException(nameof(node));
+
+        var stack = new Stack<T>();
+
+        stack.Push(node);
+
+        while (stack.Any())
+        {
+            var pop = stack.Pop();
+
+            yield return pop;
+
+            foreach (var item in pop.Reverse())
+            {
+                stack.Push((T)item);
             }
         }
     }
