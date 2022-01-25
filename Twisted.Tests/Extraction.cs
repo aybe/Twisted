ee9226e047a8c553f6c9e4ddc4a70b09a25ca3c2
aybe <aybe@users.noreply.github.com>
2022-01-25 15:44:07 +0100

    Split the printing and extraction of nodes/polygons into different tests

---
+++ Twisted.Tests/Extraction.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twisted.PS;
+
+// ReSharper disable StringLiteralTypo
+
+namespace Twisted.Tests;
+
+[TestClass]
+public sealed class Extraction
+// massive extraction of all nodes and polygons sorted for further analysis
+{
+    [TestMethod]
+    public void ExtractNodes()
+    {
+        var path = GetPath(@".twisted\TM1PSJAP");
+
+        var root = GetNodes(path);
+
+        var dictionary = TreeNodeUtility.GetDictionaryOfNodes(root);
+
+        ExtractBinaryObjects(dictionary, Path.Combine(path, "nodes.bin"));
+    }
+
+    [TestMethod]
+    public void ExtractPolygons()
+    {
+        var path = GetPath(@".twisted\TM1PSJAP");
+
+        var root = GetNodes(path);
+
+        var dictionary = TreeNodeUtility.GetDictionaryOfPolygons(root);
+
+        ExtractBinaryObjects(dictionary, Path.Combine(path, "polygons.bin"));
+    }
+
+    private static void ExtractBinaryObjects(Dictionary<Type, List<IBinaryObject>> dictionary, string path)
+    {
+        if (dictionary == null)
+            throw new ArgumentNullException(nameof(dictionary));
+
+        if (string.IsNullOrWhiteSpace(path))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(path));
+
+        var length = dictionary.Values.SelectMany(s => s).Max(s => s.Length) + 4;
+
+        using var stream = File.Create(path);
+
+        foreach (var binaryObject in dictionary.Values.SelectMany(s => s))
+        {
+            var data = binaryObject.GetObjectData();
+
+            stream.Write(data);
+
+            for (var i = 0; i < length - data.Length; i++)
+            {
+                stream.WriteByte(0xCD);
+            }
+        }
+    }
+
+    private static TempNode GetNodes(string directory)
+    {
+        if (string.IsNullOrWhiteSpace(directory))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(directory));
+
+        var root = new TempNode();
+
+        var files = Directory.GetFiles(directory, "*.DMD", SearchOption.AllDirectories);
+
+        foreach (var file in files)
+        {
+            using var reader = new BinaryReader(File.OpenRead(file));
+
+            var dmd = new DMD(reader);
+
+            root.Add(dmd);
+        }
+
+        return root;
+    }
+
+    private static string GetPath(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(path));
+
+        var info = new DirectoryInfo(Environment.CurrentDirectory);
+
+        while (info.Parent != null)
+        {
+            var combine = Path.Combine(info.FullName, path);
+
+            if (Directory.Exists(combine))
+            {
+                return combine;
+            }
+
+            info = info.Parent;
+        }
+
+        throw new DirectoryNotFoundException();
+    }
+
+    private class TempNode : TreeNode
+    {
+    }
+}
---
+++ Twisted.Tests/TreeNodeUtility.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Twisted.Extensions;
+using Twisted.PS;
+using Twisted.PS.Polygons;
+
+namespace Twisted.Tests;
+
+public class TreeNodeUtility
+{
+    public static Dictionary<Type, List<IBinaryObject>> GetDictionaryOfNodes(TreeNode treeNode)
+    {
+        if (treeNode == null)
+            throw new ArgumentNullException(nameof(treeNode));
+
+        var dictionary = new Dictionary<Type, List<IBinaryObject>>();
+
+        foreach (var grouping in treeNode.TraverseDfsPreOrder().OfType<IBinaryObject>().GroupBy(s => s.GetType()).OrderBy(s => s.Key.Name))
+        {
+            var objects = grouping.OrderBy(s => s.GetObjectData(), ArrayComparer<byte>.Instance).ToList();
+
+            dictionary.Add(grouping.Key, objects);
+        }
+
+        return dictionary;
+    }
+
+    public static Dictionary<Type, List<IBinaryObject>> GetDictionaryOfPolygons(TreeNode treeNode)
+    {
+        if (treeNode == null)
+            throw new ArgumentNullException(nameof(treeNode));
+
+        var comparer = new DelegateEqualityComparer<Polygon>((x, y) =>
+        {
+            if (x == null && y == null)
+                return true;
+
+            if (x == null || y == null)
+                return false;
+
+            var equals = EqualityComparer<long>.Default.Equals(x.Position, y.Position);
+
+            return equals;
+        }, s => s.Position.GetHashCode());
+
+        var polygons1 = treeNode.TraverseDfsPreOrder().OfType<DMDNode00FF>().SelectMany(s => s.Polygons).ToArray();
+        var polygons2 = polygons1.Distinct(comparer).ToArray();
+
+        var dictionary = new Dictionary<Type, List<IBinaryObject>>();
+
+        foreach (var grouping in polygons2.ToLookup(s => s.GetType()))
+        {
+            var polygons3 = grouping.OrderBy(s => s.GetObjectData(), ArrayComparer<byte>.Instance).Cast<IBinaryObject>().ToList();
+
+            dictionary.Add(grouping.Key, polygons3);
+        }
+
+        return dictionary;
+    }
+}
