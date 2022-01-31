d081fa59d9e75122088114c6da81287d7e8aeef6
aybe <aybe@users.noreply.github.com>
2022-01-31 12:48:55 +0100

    Add project generation postprocessor and settings UI

---
+++ Assets/Scripts/Twisted/Editor/ProjectGeneration.cs
@@ -0,0 +1,80 @@
+﻿#pragma warning disable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using UnityEditor;
+
+namespace Twisted.Editor
+{
+    internal sealed class ProjectGeneration : AssetPostprocessor
+    {
+        private static string OnGeneratedCSProject(string path, string content)
+        {
+            var document = XDocument.Parse(content);
+
+            var root = document.Root;
+            if (root is null)
+                throw new ArgumentNullException(nameof(root));
+
+            var ns = root.GetDefaultNamespace();
+
+            var propertyGroupName = ns + "PropertyGroup";
+
+            var propertyGroup = root.Descendants(propertyGroupName).FirstOrDefault();
+
+            if (propertyGroup == null)
+            {
+                root.Add(propertyGroup = new XElement(propertyGroupName));
+            }
+
+            var settings = ProjectGenerationSettings.instance;
+
+            if (settings.Nullable)
+            {
+                var nullableName = ns + "Nullable";
+
+                var nullable = root.Descendants(nullableName).FirstOrDefault();
+
+                if (nullable == null)
+                {
+                    propertyGroup.Add(nullable = new XElement(nullableName));
+                }
+
+                nullable.Value = "enable";
+
+                var noWarnName = ns + "NoWarn";
+
+                var noWarns = root.Descendants(propertyGroupName).Descendants(noWarnName);
+
+                foreach (var noWarn in noWarns)
+                {
+                    var set = new HashSet<string>(noWarn.Value.Split(';'))
+                    {
+                        "8632" // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context
+                    };
+
+                    noWarn.Value = string.Join(";", set);
+                }
+            }
+
+            // ReSharper disable once ConvertToUsingDeclaration
+            using (var writer = new Utf8StringWriter())
+            {
+                document.Save(writer);
+
+                content = writer.ToString();
+            }
+
+            return content;
+        }
+
+        private sealed class Utf8StringWriter : StringWriter
+        {
+            public override Encoding Encoding => Encoding.UTF8;
+        }
+    }
+}
---
+++ Assets/Scripts/Twisted/Editor/ProjectGenerationSettings.cs
@@ -0,0 +1,97 @@
+﻿using Unity.CodeEditor;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Twisted.Editor
+{
+    [FilePath("ProjectSettings/" + nameof(ProjectGenerationSettings) + ".asset", FilePathAttribute.Location.ProjectFolder)]
+    internal sealed class ProjectGenerationSettings : ScriptableSingleton<ProjectGenerationSettings>
+    {
+        private const string Header = "Project Generation";
+
+        [Tooltip("Enables '#nullable enable' at project-level and disables CS8632 warnings.")]
+        public bool Nullable;
+
+        private void OnEnable()
+        {
+            hideFlags &= ~HideFlags.NotEditable;
+        }
+
+        [SettingsProvider]
+        public static SettingsProvider GetSettingsProvider()
+        {
+            var provider = new SettingsProvider($"Project/{Header}", SettingsScope.Project)
+            {
+                label = Header,
+                activateHandler = (context, root) =>
+                {
+                    if (!string.IsNullOrWhiteSpace(context))
+                    {
+                        // TODO actually, only IMGUI has the ability to highlight text
+                    }
+
+                    var container = new VisualElement
+                    {
+                        style =
+                        {
+                            marginLeft = 1.0f,
+                            marginTop = 9.0f
+                        }
+                    };
+
+                    root.Add(container);
+
+                    var title = new Label
+                    {
+                        text = Header,
+                        style =
+                        {
+                            fontSize = 19.0f,
+                            unityFontStyleAndWeight = FontStyle.Bold,
+                            marginBottom = 12.0f
+                        }
+                    };
+
+                    container.Add(title);
+
+                    var properties = new VisualElement
+                    {
+                        style =
+                        {
+                            flexDirection = FlexDirection.Column
+                        }
+                    };
+
+                    container.Add(properties);
+
+                    var button = new Button(() => CodeEditor.CurrentEditor.SyncAll())
+                    {
+                        text = "Regenerate project files",
+                        style =
+                        {
+                            alignSelf = Align.FlexStart,
+                            marginTop = 12.0f,
+                            paddingLeft = 12.0f,
+                            paddingRight = 12.0f
+                        }
+                    };
+
+                    container.Add(button);
+
+                    var settings = new SerializedObject(instance);
+
+                    properties.Add(new PropertyField(settings.FindProperty(nameof(Nullable))));
+
+                    root.Bind(settings);
+
+                    root.RegisterCallback<SerializedPropertyChangeEvent>(_ => { instance.Save(true); });
+                },
+                keywords = new[] { "project", "C#", "csharp", "csproj", "nullable" }
+            };
+
+            return provider;
+        }
+    }
+}
