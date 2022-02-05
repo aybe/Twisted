bc7b5e40b08f133398b2bd396b3f474eb7fe6ec2
aybe <aybe@users.noreply.github.com>
2022-02-06 00:56:05 +0100

    Make DMD preview render polygons individually

---
+++ Assets/Scripts/Twisted/DMDPreviewBatch.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Twisted
+{
+    internal sealed class DMDPreviewBatch : MonoBehaviour
+    {
+        public List<string> Polygons;
+    }
+}
--- Assets/Scripts/Twisted/DMDViewerPreview.cs
+++ Assets/Scripts/Twisted/DMDViewerPreview.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Twisted.Extensions;
 using Twisted.PS;
 using Unity.VisualScripting;
@@ -11,100 +12,115 @@ namespace Twisted
 {
     [ExecuteAlways]
     [Singleton(Automatic = true)]
-    [RequireComponent(typeof(MeshFilter), typeof(MeshCollider), typeof(MeshRenderer))]
     public sealed class DMDViewerPreview : MonoBehaviour, ISingleton
     {
-        public void SetNode(DMDNode00FF? node)
+        public void SetNode(DMDNode00FF? node, bool split = true)
         {
-            var meshFilter   = GetComponent<MeshFilter>();
-            var meshCollider = GetComponent<MeshCollider>();
-            var meshRenderer = GetComponent<MeshRenderer>();
+            while (transform.childCount > 0)
+            {
+                var tr = transform.GetChild(0);
+                var mf = tr.GetComponent<MeshFilter>();
+                var mr = tr.GetComponent<MeshRenderer>();
+                var mc = tr.GetComponent<MeshCollider>();
 
-            // welcome to Unity... when a property to a reference doesn't behave as it normally should,
-            // destroying a reference simply isn't enough, you have to nullify that property as well...
-            // or you'll scratch your head on why it works only once and that cryptic 'Missing' message
-
-            DestroyImmediate(meshFilter.sharedMesh);
-            DestroyImmediate(meshCollider.sharedMesh);
-            DestroyImmediate(meshRenderer.sharedMaterial);
-
-            meshFilter.sharedMesh       = null;
-            meshCollider.sharedMesh     = null;
-            meshRenderer.sharedMaterial = null;
+                DestroyImmediate(mf.sharedMesh);
+                DestroyImmediate(mc.sharedMesh);
+                DestroyImmediate(mr.sharedMaterial);
+                DestroyImmediate(tr.gameObject);
+            }
 
             if (node == null)
                 return;
 
-            var polygons = node.Polygons;
+            var lists = split ? node.Polygons.Select(s => new[] { s }).ToArray() : new[] { node.Polygons };
 
-            Debug.Log($"Building {node}...");
+            var index = 0;
 
-            Debug.Log($"Model has {polygons.Count} polygons.");
-
-            var vertices   = new List<Vector3>();
-            var colors     = new List<Color>();
-            var indices    = new List<int>();
-            var dictionary = new Dictionary<Type, Color>();
-
-            using var scope = new RandomStateScope(1234);
-
-            var winding = new[] { 2, 1, 0, 2, 3, 1 };
-            var matrix  = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(-90.0f, 0.0f, 0.0f), Vector3.one * 0.01f);
-
-            foreach (var polygon in polygons)
+            using (new RandomStateScope(1234))
             {
-                Color color;
+                var dictionary = new Dictionary<Type, Color>();
+                var winding    = new[] { 2, 1, 0, 2, 3, 1 };
+                var matrix     = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(-90.0f, 0.0f, 0.0f), Vector3.one * 0.01f);
 
-                var type = polygon.GetType();
+                foreach (var list in lists)
+                {
+                    var vertices = new List<Vector3>();
+                    var colors   = new List<Color>();
+                    var indices  = new List<int>();
 
-                if (dictionary.ContainsKey(type))
-                {
-                    color = dictionary[type];
-                }
-                else
-                {
-                    color = Random.ColorHSV(0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f);
-                    dictionary.Add(type, color);
-                }
-
-                for (var i = 0; i < polygon.Vertices.Count / 2; i++)
-                {
-                    for (var j = 0; j < 3; j++)
+                    foreach (var polygon in list)
                     {
-                        var k = i * 3 + j;
-                        var l = winding[k];
-                        var m = polygon.Vertices[l];
-                        var n = m.ToVector3();
-                        var o = matrix.MultiplyPoint(n);
-                        vertices.Add(o);
-                        colors.Add(color);
-                        indices.Add(indices.Count);
+                        Color color;
+
+                        var type = polygon.GetType();
+
+                        if (dictionary.ContainsKey(type))
+                        {
+                            color = dictionary[type];
+                        }
+                        else
+                        {
+                            color = Random.ColorHSV(0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f);
+                            dictionary.Add(type, color);
+                        }
+
+                        for (var i = 0; i < polygon.Vertices.Count / 2; i++)
+                        {
+                            for (var j = 0; j < 3; j++)
+                            {
+                                var k = i * 3 + j;
+                                var l = winding[k];
+                                var m = polygon.Vertices[l];
+                                var n = m.ToVector3();
+                                var o = matrix.MultiplyPoint(n);
+                                vertices.Add(o);
+                                colors.Add(color);
+                                indices.Add(indices.Count);
+                            }
+                        }
                     }
+
+                    var meshName = split ? $"Polygon {index++} @ {list.Single().Position} ({list.Single().GetType().Name})" : "Polygons";
+
+                    var mesh = new Mesh
+                    {
+                        name      = meshName,
+                        hideFlags = HideFlags.HideAndDontSave,
+                        vertices  = vertices.ToArray(),
+                        colors    = colors.ToArray(),
+                        triangles = indices.ToArray()
+                    };
+
+                    var go = gameObject.CreateChild(meshName);
+                    var mf = go.AddComponent<MeshFilter>();
+                    var mc = go.AddComponent<MeshCollider>();
+                    var mr = go.AddComponent<MeshRenderer>();
+                    var pb = go.AddComponent<DMDPreviewBatch>();
+
+                    mf.sharedMesh     = mesh;
+                    mc.sharedMesh     = mesh;
+                    mr.sharedMaterial = new Material(Shader.Find("Twisted/DMDViewer"));
+                    pb.Polygons       = list.Select(s => string.Concat(s.GetObjectData().Select(t => t.ToString("X2")))).ToList();
                 }
             }
-
-            var mesh = new Mesh
-            {
-                hideFlags = HideFlags.HideAndDontSave, name = node.ToString(),
-                vertices  = vertices.ToArray(),
-                colors    = colors.ToArray(),
-                triangles = indices.ToArray()
-            };
-
-            meshFilter.sharedMesh = mesh;
-
-            meshCollider.sharedMesh = mesh;
-
-            meshRenderer.sharedMaterial = new Material(Shader.Find("Twisted/DMDViewer"));
 
             // frame the object but unlike Unity, make it pleasant
 
             var view = SceneView.lastActiveSceneView;
 
-            if (view != null)
+            if (view == null)
+                return;
+
+            var renderers = gameObject.GetComponentsInChildren<Renderer>();
+            var renderer1 = renderers.FirstOrDefault();
+            var bounds    = renderer1 != null ? renderer1.bounds : new Bounds();
+
+            foreach (var current in renderers)
             {
-                view.Frame(meshRenderer.bounds, false);
+                bounds.Encapsulate(current.bounds);
             }
+
+            view.Frame(bounds, false);
         }
     }
 }
