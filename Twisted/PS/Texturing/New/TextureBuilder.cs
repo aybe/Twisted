0f96c33d7a926b0ec37e1f8bf4f1c37ff924f2e4
aybe <aybe@users.noreply.github.com>
2022-02-08 23:05:17 +0100

    Add initial stuff for texture atlas

---
+++ Twisted/PS/Texturing/New/TextureBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Twisted.PS.Texturing.New
+{
+    public class TextureBuilder
+    {
+        private FrameBufferObject FrameBufferObject;
+
+        public static void GetTexture(TexturePageFormatKey key, FrameBufferObject obj)
+        {
+            // BUG use already written types?
+            // BUG use 16-bit units on framebuffer
+
+            var pageX = key.Page.X;
+            var pageY = key.Page.Y;
+
+            var pageWidth = key.PageFormat switch
+            {
+                TexturePageFormat.Indexed4 => 64,
+                TexturePageFormat.Indexed8 => 128,
+                TexturePageFormat.Direct15 => 256,
+                TexturePageFormat.Direct24 => throw new NotSupportedException(),
+                _ => throw new NotSupportedException()
+            };
+
+            switch (key.PageFormat)
+            {
+                case TexturePageFormat.Indexed4:
+                    var bytes = obj.Pixels as byte[] ?? obj.Pixels.ToArray();
+                    var shorts = new short[obj.Pixels.Count / 2];
+                    var index = pageY * 1024 + pageX;
+
+
+                    break;
+                case TexturePageFormat.Indexed8:
+                    break;
+                case TexturePageFormat.Direct15:
+                    break;
+                case TexturePageFormat.Direct24:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Twisted/PS/Texturing/New/TexturePageFormatKey.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Twisted.PS.Texturing.New
+{
+    public readonly struct TexturePageFormatKey : IEquatable<TexturePageFormatKey>
+    {
+        public TexturePage Page { get; }
+
+        public TexturePageFormat PageFormat { get; }
+
+        public TexturePageFormatKey(TexturePage page, TexturePageFormat pageFormat)
+        {
+            Page       = page;
+            PageFormat = pageFormat;
+        }
+
+        #region IEquatable
+
+        public bool Equals(TexturePageFormatKey other)
+        {
+            return Page.Equals(other.Page) && PageFormat == other.PageFormat;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TexturePageFormatKey other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Page, (int)PageFormat);
+        }
+
+        public static bool operator ==(TexturePageFormatKey left, TexturePageFormatKey right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(TexturePageFormatKey left, TexturePageFormatKey right)
+        {
+            return !left.Equals(right);
+        }
+
+        #endregion
+    }
+}
