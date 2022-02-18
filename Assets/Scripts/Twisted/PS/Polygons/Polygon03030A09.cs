cc48ad622d2770cfa2a82a39f4e4e0add01b6808
aybe <aybe@users.noreply.github.com>
2022-02-18 08:37:36 +0100

    Update Polygon03030A09.cs
    Fix incorrect colors

--- Assets/Scripts/Twisted/PS/Polygons/Polygon03030A09.cs
+++ Assets/Scripts/Twisted/PS/Polygons/Polygon03030A09.cs
@@ -4,7 +4,6 @@ namespace Twisted.PS.Polygons
 {
     internal sealed class Polygon03030A09 : PolygonG3
         // TODO unknown int32 @ 12
-        // TODO unknown int32 @ 28
         // TODO unknown int32 @ 32
         // TODO unknown int32 @ 36
     {
@@ -13,9 +12,9 @@ namespace Twisted.PS.Polygons
         {
         }
 
-        protected override int? ColorElements { get; } = 4;
+        protected override int? ColorElements { get; } = 3;
 
-        protected override int? ColorPosition { get; } = 12;
+        protected override int? ColorPosition { get; } = 16;
 
         protected override int? ColorType { get; } = 0x34;
     }
