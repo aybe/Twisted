777dfcd379cd10bf2e40254772be2c62d7c9bd17
aybe <aybe@users.noreply.github.com>
2022-03-02 21:59:08 +0100

    Add TreeNode.PrintHierarchyBackward

--- Assets/Scripts/Unity.Extensions/TreeNodeExtensions.cs
+++ Assets/Scripts/Unity.Extensions/TreeNodeExtensions.cs
@@ -1,11 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Unity.Extensions
 {
     public static class TreeNodeExtensions
     {
+        public static string PrintHierarchyBackward(this TreeNode node)
+        {
+            if (node is null)
+                throw new ArgumentNullException(nameof(node));
+
+            var builder = new StringBuilder();
+
+            var current = node;
+
+            while (current != null)
+            {
+                if (builder.Length != 0)
+                {
+                    builder.Insert(0, Environment.NewLine);
+                }
+
+                builder.Insert(0, current);
+
+                if (current.Parent != null)
+                {
+                    builder.Insert(0, "└───");
+
+                    for (var i = 0; i < current.Depth - 1; i++)
+                    {
+                        builder.Insert(0, "    ");
+                    }
+                }
+
+                current = current.Parent;
+            }
+
+            var result = builder.ToString();
+
+            return result;
+        }
+
         public static IEnumerable<TreeNode> TraverseBfs(this TreeNode node)
         {
             if (node == null)
