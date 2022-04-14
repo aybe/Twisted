41c8eb398f4c59aa0157528a00f9ad2e60b5e7ec
aybe <aybe@users.noreply.github.com>
2022-04-14 23:46:31 +0200

    Create a generic tree view!

---
+++ Assets/Editor/GenericTreeView.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Unity.Extensions;
+using UnityEngine.UIElements;
+
+namespace Editor
+{
+    public class GenericTreeView<T> : MultiColumnTreeView, IDisposable where T : TreeNode
+    {
+        private readonly GenericTreeViewColumn<T>[] Columns;
+
+        private T? Root;
+
+        public GenericTreeView(GenericTreeViewColumn<T>[] columns)
+        {
+            if (columns == null)
+                throw new ArgumentNullException(nameof(columns));
+
+            if (columns.Length == 0)
+                throw new ArgumentException("Value cannot be an empty collection.", nameof(columns));
+
+            Columns = columns;
+
+            foreach (var column in columns)
+            {
+                this.columns.Add(column.GetColumn());
+            }
+
+            sortingEnabled = true;
+
+            columnSortingChanged += OnColumnSortingChanged;
+        }
+
+        public void Dispose()
+        {
+            columnSortingChanged -= OnColumnSortingChanged;
+        }
+
+        public void SetRoot(T? node)
+        {
+            Root = node;
+
+            var items = GetRootItems();
+
+            SetRootItems(items);
+
+            Rebuild();
+        }
+
+        private void OnColumnSortingChanged()
+            // BUG: Unity code monkeys raise this N headers + 2 times in a row... unless you hold a fucking modifier!
+        {
+            // deep sorting screws ids and thus expanded nodes, save this info to restore it later on new hierarchy
+
+            SaveExpandedNodes(out var map1, out var map2);
+
+            SetRootItems(GetRootItems());
+
+            LoadExpandedNodes(map1, map2);
+
+            RefreshItems();
+        }
+
+        private void SaveExpandedNodes(out IReadOnlyDictionary<object, int> map1, out IReadOnlyDictionary<int, bool> map2)
+        {
+            var dictionary1 = new Dictionary<object, int>();
+            var dictionary2 = new Dictionary<int, bool>();
+
+            var controller = viewController;
+
+            var ids = controller.GetAllItemIds();
+
+            foreach (var id in ids)
+            {
+                var index = controller.GetIndexForId(id);
+
+                if (index is -1)
+                    continue;
+
+                var key1 = controller.GetItemForIndex(index);
+                var key2 = controller.IsExpanded(id);
+                dictionary1.Add(key1, id);
+                dictionary2.Add(id, key2);
+            }
+
+            map1 = new ReadOnlyDictionary<object, int>(dictionary1);
+            map2 = new ReadOnlyDictionary<int, bool>(dictionary2);
+        }
+
+        private void LoadExpandedNodes(IReadOnlyDictionary<object, int> map1, IReadOnlyDictionary<int, bool> map2)
+        {
+            if (map1 == null)
+                throw new ArgumentNullException(nameof(map1));
+
+            if (map2 == null)
+                throw new ArgumentNullException(nameof(map2));
+
+            var controller = viewController;
+
+            controller.CollapseAll();
+
+            var ids = controller.GetAllItemIds();
+
+            foreach (var id in ids)
+            {
+                var index = controller.GetIndexForId(id);
+
+                if (index is -1)
+                    continue;
+
+                var o = controller.GetItemForIndex(index);
+
+                if (!map1.TryGetValue(o, out var i))
+                    continue;
+
+                if (!map2.TryGetValue(i, out var b))
+                    continue;
+
+                if (b)
+                {
+                    controller.ExpandItem(id, false, false);
+                }
+            }
+        }
+
+        private List<TreeViewItemData<T>> GetRootItems()
+        {
+            var list = new List<TreeViewItemData<T>>();
+
+            if (Root is null)
+            {
+                return list;
+            }
+
+            var stack = new Stack<(T Node, TreeViewItemData<T>? Container)>();
+
+            stack.Push((Root, null));
+
+            var descriptions = sortedColumns as SortColumnDescription[] ?? sortedColumns.ToArray();
+
+            var dictionary = descriptions.ToDictionary(s => s.columnName, s => Columns.Single(t => t.Name == s.columnName));
+
+            var id = 0;
+
+            while (stack.Count > 0)
+            {
+                var (node, container) = stack.Pop();
+
+                var data = new TreeViewItemData<T>(id++, node);
+
+                if (container is null)
+                {
+                    list.Add(data);
+                }
+                else
+                {
+                    ((IList<TreeViewItemData<T>>)container.Value.children).Add(data);
+                }
+
+                var children = node.Cast<T>().Reverse();
+
+                foreach (var description in descriptions)
+                {
+                    var column = dictionary[description.columnName];
+                    var getter = column.ValueGetter ?? throw new InvalidOperationException();
+                    var order  = description.direction is SortDirection.Descending;
+
+                    children = children.Sort(getter, null, order);
+                }
+
+                foreach (var child in children)
+                {
+                    stack.Push((child, data));
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Assets/Editor/GenericTreeViewColumn.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+using JetBrains.Annotations;
+using Unity.Extensions;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Editor
+{
+    public class GenericTreeViewColumn<T> where T : TreeNode
+    {
+        public GenericTreeViewColumn()
+        {
+            Optional    = true;
+            Resizable   = true;
+            Sortable    = true;
+            Visible     = true;
+            Stretchable = false;
+
+            static T GetNode(VisualElement element, int index)
+            {
+                if (element is null)
+                    throw new ArgumentNullException(nameof(element));
+
+                var view = element.GetParent<GenericTreeView<T>>() ?? throw new InvalidOperationException();
+                var item = view.viewController.GetItemForIndex(index);
+                var node = item as T ?? throw new InvalidOperationException();
+
+                return node;
+            }
+
+            static VisualElement GetLabel(TextAnchor anchor)
+            {
+                return new Label
+                {
+                    style =
+                    {
+                        flexGrow       = 1,
+                        unityTextAlign = anchor,
+                        overflow       = Overflow.Hidden,
+                        textOverflow   = TextOverflow.Ellipsis
+                    }
+                };
+            }
+
+            VisualElement MakeHeader()
+            {
+                return GetLabel(TextAnchor.MiddleLeft);
+            }
+
+            void BindHeader(VisualElement element)
+            {
+                ((Label)element).text = Title;
+            }
+
+            void UnbindHeader(VisualElement element)
+            {
+                ((Label)element).text = default;
+            }
+
+            void DestroyHeader(VisualElement element)
+            {
+            }
+
+            VisualElement MakeCell()
+            {
+                return GetLabel(TextAnchor.MiddleLeft);
+            }
+
+            void BindCell(VisualElement element, int index)
+            {
+                var node = GetNode(element, index);
+                var data = ValueGetter?.Invoke(node);
+                var text = ValueFormatter?.Invoke(data) ?? data?.ToString();
+                ((Label)element).text = text;
+            }
+
+            void UnbindCell(VisualElement element, int index)
+            {
+                ((Label)element).text = default;
+            }
+
+            void DestroyCell(VisualElement element)
+            {
+            }
+
+            HeaderMake    = MakeHeader;
+            HeaderBind    = BindHeader;
+            HeaderUnbind  = UnbindHeader;
+            HeaderDestroy = DestroyHeader;
+            CellMake      = MakeCell;
+            CellBind      = BindCell;
+            CellUnbind    = UnbindCell;
+            CellDestroy   = DestroyCell;
+        }
+
+        private Column Column { get; } = new();
+
+        [PublicAPI]
+        public string Name
+        {
+            get => Column.name;
+            set => Column.name = value;
+        }
+
+        [PublicAPI]
+        public string Title
+        {
+            get => Column.title;
+            set => Column.title = value;
+        }
+
+        [PublicAPI]
+        public Background Icon
+        {
+            get => Column.icon;
+            set => Column.icon = value;
+        }
+
+        [PublicAPI]
+        public bool Visible
+        {
+            get => Column.visible;
+            set => Column.visible = value;
+        }
+
+        [PublicAPI]
+        public Length Width
+        {
+            get => Column.width;
+            set => Column.width = value;
+        }
+
+        [PublicAPI]
+        public Length MinWidth
+        {
+            get => Column.minWidth;
+            set => Column.minWidth = value;
+        }
+
+        [PublicAPI]
+        public Length MaxWidth
+        {
+            get => Column.maxWidth;
+            set => Column.maxWidth = value;
+        }
+
+        [PublicAPI]
+        public bool Sortable
+        {
+            get => Column.sortable;
+            set => Column.sortable = value;
+        }
+
+        [PublicAPI]
+        public bool Stretchable
+        {
+            get => Column.stretchable;
+            set => Column.stretchable = value;
+        }
+
+        [PublicAPI]
+        public bool Optional
+        {
+            get => Column.optional;
+            set => Column.optional = value;
+        }
+
+        [PublicAPI]
+        public bool Resizable
+        {
+            get => Column.resizable;
+            set => Column.resizable = value;
+        }
+
+        [PublicAPI]
+        public Func<VisualElement> HeaderMake
+        {
+            get => Column.makeHeader;
+            set => Column.makeHeader = value;
+        }
+
+        [PublicAPI]
+        public Action<VisualElement> HeaderBind
+        {
+            get => Column.bindHeader;
+            set => Column.bindHeader = value;
+        }
+
+        [PublicAPI]
+        public Action<VisualElement> HeaderUnbind
+        {
+            get => Column.unbindHeader;
+            set => Column.unbindHeader = value;
+        }
+
+        [PublicAPI]
+        public Action<VisualElement> HeaderDestroy
+        {
+            get => Column.destroyHeader;
+            set => Column.destroyHeader = value;
+        }
+
+        [PublicAPI]
+        public Func<VisualElement> CellMake
+        {
+            get => Column.makeCell;
+            set => Column.makeCell = value;
+        }
+
+        [PublicAPI]
+        public Action<VisualElement, int> CellBind
+        {
+            get => Column.bindCell;
+            set => Column.bindCell = value;
+        }
+
+        [PublicAPI]
+        public Action<VisualElement, int> CellUnbind
+        {
+            get => Column.unbindCell;
+            set => Column.unbindCell = value;
+        }
+
+        [PublicAPI]
+        public Action<VisualElement> CellDestroy
+        {
+            get => Column.destroyCell;
+            set => Column.destroyCell = value;
+        }
+
+        [PublicAPI]
+        public Func<T, object?>? ValueGetter { get; set; }
+
+        [PublicAPI]
+        public Func<object?, string?>? ValueFormatter { get; set; }
+
+        [PublicAPI]
+        public Func<object?, IComparer>? ValueComparer { get; set; }
+
+        internal Column GetColumn()
+        {
+            return Column;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}";
+        }
+    }
+}
