54d6eb620cda364842228f86b543167cd5fed02f
aybe <aybe@users.noreply.github.com>
2022-04-14 23:48:51 +0200

    Use that new generic tree view which declutter our window in the mean time

--- Assets/Editor/DMDViewer.cs
+++ Assets/Editor/DMDViewer.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using Twisted.Graphics;
-using Unity.Extensions;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -33,13 +32,99 @@ namespace Editor
         [SerializeField]
         private VisualTreeAsset VisualTreeAsset = null!;
 
-        private MultiColumnTreeView TreeView => rootVisualElement.Q<MultiColumnTreeView>();
+        private GenericTreeView<DMDNode> TreeView = null!;
+
+        private void OnEnable()
+        {
+            TreeView = CreateTreeView();
+
+            [SuppressMessage("ReSharper", "UnusedParameter.Local")]
+            static GenericTreeView<DMDNode> CreateTreeView()
+            {
+                var column1 = new GenericTreeViewColumn<DMDNode>
+                {
+                    Title          = "Name",
+                    Name           = "Name",
+                    ValueGetter    = s => s.GetType().Name,
+                    ValueComparer  = s => Comparer<string>.Default,
+                    ValueFormatter = s => $"{s}",
+                    Width          = 200.0f
+                };
+
+                var column2 = new GenericTreeViewColumn<DMDNode>
+                {
+                    Title          = "Type 1",
+                    Name           = "Type 1",
+                    ValueGetter    = s => s.NodeType,
+                    ValueComparer  = s => Comparer<uint>.Default,
+                    ValueFormatter = s => s is uint u ? $"0x{(u >> 16) & 0xFFFF:X8}" : null,
+                    Width          = 100.0f
+                };
+
+                var column3 = new GenericTreeViewColumn<DMDNode>
+                {
+                    Title          = "Type 2",
+                    Name           = "Type 2",
+                    ValueGetter    = s => s.NodeType,
+                    ValueComparer  = s => Comparer<uint>.Default,
+                    ValueFormatter = s => s is uint u ? $"0x{(u >> 00) & 0xFFFF:X8}" : null,
+                    Width          = 100.0f
+                };
+
+                var column4 = new GenericTreeViewColumn<DMDNode>
+                {
+                    Title          = "Position",
+                    Name           = "Position",
+                    ValueGetter    = s => s.Position,
+                    ValueComparer  = s => Comparer<long>.Default,
+                    ValueFormatter = s => $"{s:N0}",
+                    Width          = 100.0f
+                };
+
+                var column5 = new GenericTreeViewColumn<DMDNode>
+                {
+                    Title          = "Length",
+                    Name           = "Length",
+                    ValueGetter    = s => s.Length,
+                    ValueComparer  = s => Comparer<long>.Default,
+                    ValueFormatter = s => $"{s:N0}",
+                    Width          = 100.0f
+                };
+
+                var column6 = new GenericTreeViewColumn<DMDNode>
+                {
+                    Title          = "Polygons",
+                    Name           = "Polygons",
+                    ValueGetter    = s => s is DMDNode00FF ff ? ff.GetPolygonsString() : "N/A",
+                    ValueComparer  = s => Comparer<string>.Default,
+                    ValueFormatter = s => $"{s}",
+                    Width          = 200.0f
+                };
+
+                var columns = new[]
+                {
+                    column1,
+                    column2,
+                    column3,
+                    column4,
+                    column5,
+                    column6
+                };
+
+                var treeView = new GenericTreeView<DMDNode>(columns);
+
+                return treeView;
+            }
+        }
 
         private void OnDisable()
         {
-            TreeView.columnSortingChanged -= OnTreeViewColumnSortingChanged;
+            //TreeView.columnSortingChanged -= OnTreeViewColumnSortingChanged;
+            TreeView.Dispose();
         }
 
+        [SuppressMessage("ReSharper", "UnusedVariable")]
+        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
         public void CreateGUI()
         {
             var root = rootVisualElement;
@@ -118,9 +203,11 @@ namespace Editor
 
             root.Bind(Model.SerializedObject);
 
+            root.Q("TreeViewHost").Add(TreeView);
+
             var sliderItemHeight = root.Q<SliderInt>();
             var searchField      = root.Q<ToolbarSearchField>();
-            var treeView         = root.Q<MultiColumnTreeView>();
+            var treeView         = TreeView;
 
             root.RegisterCallback<KeyDownEvent>(evt =>
             {
@@ -161,135 +248,6 @@ namespace Editor
             {
                 InitializeTreeView();
             }
-            else
-            {
-                TreeView.visible = false; // stupid tree view greets you with a NRE when clicked otherwise
-                // furthermore that shows an empty block where columns should be
-            }
-
-            TreeView.sortingEnabled = true;
-
-            TreeView.columnSortingChanged += OnTreeViewColumnSortingChanged;
-        }
-
-        private void OnTreeViewColumnSortingChanged()
-            // BUG code monkeys at Unity will raise this event N clicked headers + 2 times in a row, how nice...
-            // that, unless you hold Ctrl or Shift... but do you think they bothered documenting this fact?
-        {
-            Debug.Log(nameof(OnTreeViewColumnSortingChanged)); // TODO delete
-
-            // multi-column sort screws ids and thus expanded nodes, do a backup
-
-            var dictionary1 = new Dictionary<object, int>();
-            var dictionary2 = new Dictionary<int, bool>();
-            var controller  = TreeView.viewController;
-            var ids1        = controller.GetAllItemIds();
-
-            foreach (var id in ids1)
-            {
-                var index = controller.GetIndexForId(id);
-                if (index is -1)
-                    continue;
-
-                var key1 = controller.GetItemForIndex(index);
-                var key2 = controller.IsExpanded(id);
-                dictionary1.Add(key1, id);
-                dictionary2.Add(id, key2);
-            }
-
-            // perform the actual multi-column deep sorting
-
-            var items = InitializeTreeViewItems(Model.DMDFactory!.DMD, TreeView.sortedColumns);
-
-            TreeView.SetRootItems(items);
-
-            // restore the state of expanded nodes
-
-            controller.CollapseAll();
-
-            var ids2 = controller.GetAllItemIds();
-
-            foreach (var id in ids2)
-            {
-                var index = controller.GetIndexForId(id);
-                if (index is -1)
-                    continue;
-
-                var o = controller.GetItemForIndex(index);
-
-                if (!dictionary1.TryGetValue(o, out var i))
-                    continue;
-
-                if (!dictionary2.TryGetValue(i, out var b))
-                    continue;
-
-                if (b)
-                {
-                    controller.ExpandItem(id, false, false);
-                }
-            }
-
-            TreeView.RefreshItems();
-        }
-
-        private static List<TreeViewItemData<DMDNode>> InitializeTreeViewItems(DMD dmd, IEnumerable<SortColumnDescription>? descriptions)
-        {
-            if (dmd is null)
-                throw new ArgumentNullException(nameof(dmd));
-
-            descriptions = descriptions as SortColumnDescription[] ?? descriptions?.ToArray();
-
-            var id    = 0;
-            var list  = new List<TreeViewItemData<DMDNode>>();
-            var stack = new Stack<(DMDNode Node, TreeViewItemData<DMDNode>? Container)>();
-
-            stack.Push((dmd, null));
-
-            while (stack.Count > 0)
-            {
-                var (node, container) = stack.Pop();
-
-                var data = new TreeViewItemData<DMDNode>(id++, node);
-
-                if (container is null)
-                {
-                    list.Add(data);
-                }
-                else
-                {
-                    ((IList<TreeViewItemData<DMDNode>>)container.Value.children).Add(data);
-                }
-
-                var children = node.Cast<DMDNode>().Reverse();
-
-                if (descriptions != null)
-                {
-                    foreach (var description in descriptions)
-                    {
-                        var column = description.columnName;
-
-                        Func<DMDNode, object> selector = column switch
-                        {
-                            ColumnNodeName     => GetNodeName,
-                            ColumnNodeType1    => GetNodeType1,
-                            ColumnNodeType2    => GetNodeType2,
-                            ColumnNodePosition => GetNodePosition,
-                            ColumnNodeLength   => GetNodeLength,
-                            ColumnNodePolygons => GetNodePolygons,
-                            _                  => throw new NotSupportedException(column)
-                        };
-
-                        children = children.Sort(selector, null, description.direction is SortDirection.Descending);
-                    }
-                }
-
-                foreach (var child in children)
-                {
-                    stack.Push((child, data));
-                }
-            }
-
-            return list;
         }
 
         private void InitializeModel()
@@ -318,163 +276,10 @@ namespace Editor
 
         private void InitializeTreeView()
         {
-            var column1 = CreateDefaultColumn(ColumnNodeName,     200.0f, GetNodeName);
-            var column2 = CreateDefaultColumn(ColumnNodeType1,    100.0f, GetNodeType1);
-            var column3 = CreateDefaultColumn(ColumnNodeType2,    100.0f, GetNodeType2);
-            var column4 = CreateDefaultColumn(ColumnNodePosition, 100.0f, GetNodePosition);
-            var column5 = CreateDefaultColumn(ColumnNodeLength,   100.0f, GetNodeLength);
-            var column6 = CreateDefaultColumn(ColumnNodePolygons, 200.0f, GetNodePolygons);
+            var dmd = Model.DMDFactory?.DMD ?? throw new InvalidOperationException();
 
-            var view = TreeView;
-
-            view.columns.Clear();
-
-            view.columns.Add(column1);
-            view.columns.Add(column2);
-            view.columns.Add(column3);
-            view.columns.Add(column4);
-            view.columns.Add(column5);
-            view.columns.Add(column6);
-
-            view.sortColumnDescriptions.Clear();
-
-            var items = InitializeTreeViewItems(Model.DMDFactory?.DMD ?? throw new InvalidOperationException(), null);
-
-            view.SetRootItems(items);
-            view.Rebuild();
-            view.ExpandRootItems();
-
-            if (view.visible is false)
-            {
-                view.visible = true;
-            }
+            TreeView.SetRoot(dmd);
+            TreeView.ExpandRootItems();
         }
-
-        private static Column CreateDefaultColumn(string header, float width, Func<DMDNode, object> getter)
-        {
-            if (string.IsNullOrWhiteSpace(header))
-                throw new ArgumentException("Value cannot be null or whitespace.", nameof(header));
-
-            if (getter is null)
-                throw new ArgumentNullException(nameof(getter));
-
-            if (width <= 0.0f)
-                throw new ArgumentOutOfRangeException(nameof(width));
-
-            static VisualElement CreateDefaultLabel(TextAnchor anchor)
-            {
-                return new Label
-                {
-                    style =
-                    {
-                        flexGrow       = 1,
-                        unityTextAlign = anchor,
-                        overflow       = Overflow.Hidden,
-                        textOverflow   = TextOverflow.Ellipsis
-                    }
-                };
-            }
-
-            VisualElement MakeHeader()
-            {
-                return CreateDefaultLabel(TextAnchor.MiddleLeft);
-            }
-
-            void BindHeader(VisualElement element)
-            {
-                ((Label)element).text = header;
-            }
-
-            void UnbindHeader(VisualElement element)
-            {
-                ((Label)element).text = default;
-            }
-
-            VisualElement MakeCell()
-            {
-                return CreateDefaultLabel(TextAnchor.MiddleLeft);
-            }
-
-            void BindCell(VisualElement element, int index)
-            {
-                ((Label)element).text = getter(GetNodeFromItem(element, index))?.ToString();
-            }
-
-            var column = new Column // stretchable sucks big time
-            {
-                name          = header,
-                title         = header,
-                icon          = default,
-                visible       = true,
-                width         = width,
-                minWidth      = 75.0f,
-                sortable      = true,
-                optional      = true,
-                resizable     = true,
-                makeHeader    = MakeHeader,
-                bindHeader    = BindHeader,
-                unbindHeader  = UnbindHeader,
-                destroyHeader = null,
-                makeCell      = MakeCell,
-                bindCell      = BindCell,
-                unbindCell    = null,
-                destroyCell   = null
-            };
-
-            return column;
-        }
-
-        private static DMDNode GetNodeFromItem(VisualElement element, int index)
-        {
-            if (element is null)
-                throw new ArgumentNullException(nameof(element));
-
-            var view = element.GetParent<MultiColumnTreeView>() ?? throw new InvalidOperationException();
-            var item = view.viewController.GetItemForIndex(index);
-            var node = item as DMDNode ?? throw new InvalidOperationException();
-
-            return node;
-        }
-
-        #region Getters
-
-        private const string ColumnNodeName     = "Name";
-        private const string ColumnNodeType1    = "Type 1";
-        private const string ColumnNodeType2    = "Type 2";
-        private const string ColumnNodePosition = "Position";
-        private const string ColumnNodeLength   = "Length";
-        private const string ColumnNodePolygons = "Polygons";
-
-        private static object GetNodeName(DMDNode node)
-        {
-            return node.GetType().Name;
-        }
-
-        private static object GetNodeType1(DMDNode node)
-        {
-            return $"0x{(node.NodeType >> 16) & 0xFFFF:X4}";
-        }
-
-        private static object GetNodeType2(DMDNode node)
-        {
-            return $"0x{(node.NodeType >> 00) & 0xFFFF:X4}";
-        }
-
-        private static object GetNodePosition(DMDNode node)
-        {
-            return node.Position;
-        }
-
-        private static object GetNodeLength(DMDNode node)
-        {
-            return node.Length;
-        }
-
-        private static object GetNodePolygons(DMDNode node)
-        {
-            return $"{(node is DMDNode00FF ff ? ff.GetPolygonsString() : "N/A")}";
-        }
-
-        #endregion
     }
 }
