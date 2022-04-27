e7f6b8f32be26f5493d3ba8a1ab5622c5dbb3576
aybe <aybe@users.noreply.github.com>
2022-04-27 05:57:32 +0200

    050B: figure out the matrix/vector and expose position

--- Assets/Scripts/Twisted/Formats/Database/DMDNode050B.cs
+++ Assets/Scripts/Twisted/Formats/Database/DMDNode050B.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace Twisted.Formats.Database
@@ -10,6 +13,8 @@ namespace Twisted.Formats.Database
 
         private readonly byte Unknown2;
 
+        private readonly Vector3 Vector1;
+
         public DMDNode050B(DMDNode? parent, BinaryReader reader)
             : base(parent, reader)
         {
@@ -18,7 +23,10 @@ namespace Twisted.Formats.Database
 
             var bytes = reader.ReadBytes(36);
 
-            // TODO 32 bytes matrix ???
+            var i16 = MemoryMarshal.Cast<byte, short>(bytes);
+            var i32 = MemoryMarshal.Cast<byte, int>(bytes);
+
+            Vector1 = MathMulVec(i16, i32);
 
             Unknown1 = bytes[32];
 
@@ -34,6 +42,25 @@ namespace Twisted.Formats.Database
 
             ReadNodes(this, reader, addresses);
         }
+
+        private static Vector3 MathMulVec(Span<short> i16, Span<int> i32) // 800FE9C4
+        {
+            var x = i32[5];
+            var y = i32[6];
+            var z = i32[7];
+
+            var u = (x * i16[0] + y * i16[1] + z * i16[2]) / 4096.0f;
+            var v = (x * i16[3] + y * i16[4] + z * i16[5]) / 4096.0f;
+            var w = (x * i16[6] + y * i16[7] + z * i16[8]) / 4096.0f;
+
+            return new Vector3(u, v, w);
+        }
+
+        public IEnumerable<Vector3> GetVectors()
+        {
+            yield return Vector1;
+        }
+
         public IEnumerable<object> GetUnknowns()
         {
             yield return Unknown1;
