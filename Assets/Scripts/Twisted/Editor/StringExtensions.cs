e6d8654e6c3099a0c84ada78dc4ba50e95adcb5f
aybe <aybe@users.noreply.github.com>
2022-04-30 00:40:27 +0200

    Add StringExtensions

---
+++ Assets/Scripts/Twisted/Editor/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Twisted.Editor
+{
+    public static class StringExtensions
+    {
+        public static string ToStringMarkup(this object? value, Color? color = null, bool? bold = null, bool? italic = null)
+        {
+            var text = value?.ToString() ?? string.Empty;
+
+            if (color.HasValue)
+            {
+                text = $"<color=#{ColorUtility.ToHtmlStringRGBA(color.Value)}>{text}</color>";
+            }
+
+            if (bold is true)
+            {
+                text = $"<b>{text}</b>";
+            }
+
+            if (italic is true)
+            {
+                text = $"<i>{text}</i>";
+            }
+
+            return text;
+        }
+    }
+}
