0b9872e95ccc3bd39bd4cd0203b6995f328c2deb
aybe <aybe@users.noreply.github.com>
2022-04-29 02:27:02 +0200

    Extract GameObjectExtensions

--- Assets/Scripts/Twisted/Editor/DMDViewerPreview.cs
+++ Assets/Scripts/Twisted/Editor/DMDViewerPreview.cs
@@ -501,24 +501,4 @@ namespace Twisted.Editor
             }
         }
     }
-
-    public static class GameObjectExtensions // TODO move
-    {
-        public static GameObject CreateChild(this GameObject parent, string? name = null, PrimitiveType? primitiveType = null)
-        {
-            if (parent == null)
-                throw new ArgumentNullException(nameof(parent));
-
-            var child = primitiveType != null ? GameObject.CreatePrimitive(primitiveType.Value) : new GameObject();
-
-            if (name is not null)
-            {
-                child.name = name;
-            }
-
-            child.transform.SetParent(parent.transform);
-
-            return child;
-        }
-    }
 }
---
+++ Assets/Scripts/Twisted/Editor/GameObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Twisted.Editor
+{
+    public static class GameObjectExtensions
+    {
+        public static GameObject CreateChild(this GameObject parent, string? name = null, PrimitiveType? primitiveType = null)
+        {
+            if (parent == null)
+                throw new ArgumentNullException(nameof(parent));
+
+            var child = primitiveType != null ? GameObject.CreatePrimitive(primitiveType.Value) : new GameObject();
+
+            if (name is not null)
+            {
+                child.name = name;
+            }
+
+            child.transform.SetParent(parent.transform);
+
+            return child;
+        }
+    }
+}
