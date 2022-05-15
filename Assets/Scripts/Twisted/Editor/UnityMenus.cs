763f38396f6c6f303660d23caa8c878513e568a4
aybe <aybe@users.noreply.github.com>
2022-05-15 12:46:19 +0200

    Regroup menu items

--- Assets/Scripts/Twisted/Editor/UnityMenus.cs
+++ Assets/Scripts/Twisted/Editor/UnityMenus.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using Twisted.Controls;
 using Twisted.Formats.Database;
 using UnityEditor;
+using UnityEngine;
 
 namespace Twisted.Editor
 {
-    internal static class UnityTwistedMenu
+    internal static partial class UnityMenus
     {
         [MenuItem("Twisted/DMD Viewer", priority = 0)]
         private static void DMDViewer()
@@ -145,6 +146,50 @@ namespace Twisted.Editor
                 }
 
                 return 0;
+            }
+        }
+    }
+
+    internal static partial class UnityMenus
+    {
+        [MenuItem("Edit/Clear Undo History", priority = 10)]
+        private static void ClearUndoHistory()
+        {
+            if (EditorUtility.DisplayDialog("Clear undo history?", "This action cannot be undone.", "Don't clear", "Clear"))
+                return;
+
+            Undo.ClearAll();
+        }
+
+        [MenuItem("CONTEXT/Transform/Select, Ping", priority = 999998)]
+        private static void TransformSelectPing(MenuCommand command)
+        {
+            var gameObject = ((Transform)command.context).gameObject;
+
+            Selection.activeGameObject = gameObject;
+
+            EditorGUIUtility.PingObject(gameObject);
+        }
+
+        [MenuItem("CONTEXT/Transform/Select, Ping, Frame", priority = 999999)]
+        private static void TransformSelectFramePing(MenuCommand command)
+        {
+            var gameObject = ((Transform)command.context).gameObject;
+
+            Selection.activeGameObject = gameObject;
+
+            EditorGUIUtility.PingObject(gameObject);
+
+            var view = SceneView.currentDrawingSceneView;
+
+            if (view == null)
+            {
+                view = SceneView.lastActiveSceneView;
+            }
+
+            if (view != null)
+            {
+                EditorApplication.delayCall += () => view.FrameSelected();
             }
         }
     }
