955c74a7ee6399d857d1dbda5a44cfa8f1abcc58
aybe <aybe@users.noreply.github.com>
2022-05-16 12:13:39 +0200

    Node info: fix incorrect sky

--- Assets/Scripts/Twisted/Formats/Database/DMDExtensions.cs
+++ Assets/Scripts/Twisted/Formats/Database/DMDExtensions.cs
@@ -61,6 +61,7 @@ namespace Twisted.Formats.Database
             var info = node.NodeType switch
             {
                 0x0107_0100 => "3D environment",
+                0x0107_0200 => "Sky",
                 0x0107_0A00 => "Sweet Tooth",
                 0x0107_1400 => "Yellow Jacket",
                 0x0107_1E00 => "Darkside",
@@ -87,7 +88,6 @@ namespace Twisted.Formats.Database
 
             info ??= role switch
             {
-                0x0002 => "Sky",
                 0x00C9 => "Bullet",
                 0x0138 => "Bullet",
                 0x00D2 => "Weapon texture",
